f10c7112f1eea1e7e164ecbdc0c82a4a111a2a7a
Juha Ala-Rantala <alarantala.juha@gmail.com>
2025-08-10 17:01:01 +0300

    Add network_mode: host to backend services
    Set network_mode to 'host' for backend-twitch and backend-web services in docker-compose.yml to enable direct host networking. Also removed an outdated comment from backend/shared/index.ts.

--- backend/shared/index.ts
+++ backend/shared/index.ts
@@ -3,5 +3,3 @@ export { PrismaClient } from "@prisma/client";
 
 // Export generated types (these will be available after running prisma generate)
 export * from "@prisma/client";
-
-// Note: .env loading should be done by each service that uses this package
